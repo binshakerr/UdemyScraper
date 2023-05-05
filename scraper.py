ae25b2c04c6285b6d828a2a9af6ae06734a8f0ba
Eslam Shaker <binshakerr@gmail.com>
2023-05-05 16:23:12 +0400

    getting items correctly

--- scraper.py
+++ scraper.py
@@ -15,17 +15,26 @@ proxies = {
 def getCourses(pageNumber, category_id):
     url = "https://www.udemy.com/api-2.0/discovery-units/all_courses/?p={}&page_size=60&subcategory=&instructional_level=&lang=&price=&duration=&closed_captions=&subs_filter_type=&label_id={}&source_page=topic_page&locale=en_US&currency=usd&navigation_locale=en_US&skip_price=true&sos=pl&fl=lbl".format(str(pageNumber), str(category_id))
     print("getting courses for page: " + str(pageNumber))
-    response = requests.get(url, proxies=proxies).json()
-    results = response["unit"]["items"]
-    return results
-
+    response = requests.get(url, proxies=proxies)
+    return response.json()
 
 def getAllCourses():
     category_id = 7380
+    pageNumber = 1
     results = []
 
-    for pageNumber in range(1, 4):
-        results += getCourses(pageNumber, category_id)
+    while True:
+        try:
+            res = getCourses(pageNumber, category_id)
+            if "detail" in res.keys(): #udemy error message
+                break 
+            results += res["unit"]["items"] 
+        except Exception as e:
+            print(e)
+            break 
+        pageNumber += 1
+        if pageNumber > 1000:
+            break
     
     df = pd.DataFrame(results)
     df.to_csv("udemyCourses.csv", index=False)
